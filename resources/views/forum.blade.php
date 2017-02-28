f65f04621a1f799a488f30408a573bb5fadbe921
Bernard Chisumo <bchisumo74@yahoo.com>
2017-02-28 18:04:06 +0000

    Styling views in forum front end

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -4,13 +4,26 @@
    @foreach($discussions as $d )
       <div class="panel panel-default">
           <div class="panel-heading">
-              <img src="{{$d->avatar}}" alt="" width="70px" height="70px">
+              <img src="{{$d->avatar}}" alt="" width="40px" height="40px">&nbsp;&nbsp;
+              <span>{{ $d->user->name }}</span>
+              <a href="{{ route('discussion', ['slug' =>$d->slug]) }}" class="btn btn-default pull-right">View</a>
           </div>
               <div class="panel-body">
-                  {{ $d->content }}
+                  <h5 class="text-center">
+                      {{ $d->title }}
+                  </h5>
+                  <p class="text-center">
+                      {{ str_limit($d->content, 50) }}
+                  </p>
               </div>
 
       </div>
+
+       <div class="panel-footer">
+           <p>
+               {{ $d->replies->count() }} Replies
+           </p>
+       </div>
 
     @endforeach
 
